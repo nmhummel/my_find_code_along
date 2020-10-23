ef16351849f312864d538e5ac4b5c9df4de8cae1
nmhummel <natalie.hummel@gmail.com>
2020-10-23 15:47:43 -0500

    commit tree 7a2eda1b665bc14d1c5aa2226757316ad14d2e5b

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_find(collection)
-
+    i = 0
+    while i < collection.length     # while number 'i' is less than collection's length
+       return collection[i] if yield(collection[i])        # yield to the following block that takes into accout collection and current number - returns value of the first number to meet our requirements
+      i = i + 1 
+    end
 end
+
+#example
+collection = (1..100).to_a  #converts numbers to an array
+my_find(collection) {|i| i % 3 == 0 and 5 == 0 }  # % means divide, so whichever number is divisible by 3 and 5 (first is 15)
